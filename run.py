dbbe81267abbb76586891a8fd203ee65b688b236
AlaYousef <>
2023-05-16 19:10:21 +0000

    Add clear screen function to clear quiz information and start quiz

--- run.py
+++ run.py
@@ -1,29 +1,53 @@
 import time
+import random
+import textwrap
+import sys
+import os
 
 def quiz_introduction():
-    print("=========================================")
-    print("|                                       |")
-    print("|     Welcome to Periodic Table Quiz    |")
-    print("|                                       |")
-    print("=========================================")
+    """
+    Function show quiz information and  instuctions
+    """
+    print("============================================================")
+    print("|                                                          |")
+    print("|             Welcome to Periodic Table Quiz               |")
+    print("|                                                          |")
+    print("============================================================")
     time.sleep(1)
-    print("\n*********** Quiz Instructions ***********")
+    print("\n******************* Quiz Instructions ********************")
     print("The quiz consists of 15 questions, with four answer options.") 
-    print("You should choose one of answer options from (1 to 4). After complete the 15 questions you will get your score")
-    time.sleep(4)
-    print("\nThe program will randomly find a value on the selected dice and calculate the score. If the sum after rolling all dices is 12 you win the game, If the sum is more than 12 so you loss the game ")
-    time.sleep(2)
+    print("You should choose one of answer options from (1 to 4)."
+    "After complete the 15 questions you will get your score")
+    time.sleep(6)
+
+    print("Press Enter to start the quiz..\n")
+    input()
+
+    
     print("\nPreparing quiz questions...")
     time.sleep(1)
     print("...")
     time.sleep(1)
     print("...")
     time.sleep(1)
-    print("...")
-    time.sleep(1)
+    print("...\n")
+    time.sleep(2)
+
+def clear_screen():
+    """
+    Clears the screen based on the user's operating system.
+    """
+    if sys.platform.startswith('win'):
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def input_value():
-    user_input  = input("Please, Enter your name:  ")
+    """
+    function that take name from user and return it
+    """
+    clear_screen()
+    user_input  = input("First, please enter your name:  ")
     return user_input
 
 def main():
