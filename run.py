1331742c6e7e31a40547e346598ea9d75a8b9b85
AlaYousef <>
2023-05-16 08:46:44 +0000

    game introduction function to provides users with game instrutions

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+def game_introduction():
+    print("=========================================")
+    print("|                                       |")
+    print("|      Welcome to the Dice Game 12      |")
+    print("|                                       |")
+    print("=========================================")
+    time.sleep(1)
+    print("***Game Instructions***")
+    print("The game consists of three dices. Each dice can only be rolled once per round.")
+    print("In each round, you should be able to choose between:")
+    print("# 1 to roll the dice 1")
+    print("# 2 to roll the dice 2")
+    print("# 3 to roll the dice 3")
+    print("# q to exit the game")
+    time.sleep(1)
+    print("Preparing the program...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+
+def main():
+    game_introduction()
+
+main()
