7978bb4a69c293bd1a41d036179f4f8fb89615b4
AlaYousef <>
2023-05-18 06:17:02 +0000

    edit run quiz function, add check_answer_input function to check answer option entered by user

--- run.py
+++ run.py
@@ -7,115 +7,115 @@ import os
 # questions list of dectionaries (question,answers,correct_answer)
 questions = [
     {
-        "question":"Which element got its name from the water it makes when burned?",
+        "question":"1. Which element got its name from the water it makes when burned?",
         "answers":
-        ["iodine", "hydrogen", "tungsten","sodium"],
-        "correct_answer": "hydrogen",
+        ["1.iodine", "2.hydrogen", "3.tungsten","4.sodium"],
+        "correct_answer": "2.hydrogen",
         "details": ("Hydrogen was first called “inflammable air” "
         " before chemist Antoine Lavoisier renamed it “water-former” (hydro + gen).")
     },
     {
-        "question": "Uranium was named after which of the following?",
+        "question": "2. Uranium was named after which of the following?",
         "answers":
-        ["a dog", "a planet", "a scientist", "a country"],
-        "correct_answer": "a planet",
+        ["1.a dog", "2.a planet", "3.a scientist", "4.a country"],
+        "correct_answer": "2.a planet",
         "details": ("Uranium was discovered in 1789, shortly after the planet Uranus was discovered." 
         " Neptunium and plutonium are also named for celestial bodies in our solar system.")
 
     },
     {
-        "question": "Which of these metals is liquid at room temperature?",
+        "question": "3. Which of these metals is liquid at room temperature?",
         "answers":
-        ["mercury", "nickel", "titanium", "zinc"],
-        "correct_answer": "mercury",
+        ["1.mercury", "2.nickel", "3.titanium", "4.zinc"],
+        "correct_answer": "1.mercury",
         "details": ("Mercury, often called “quicksilver,” remains a liquid until cooled to about −40 °F."
         " Bromine is the only other element that is liquid at room temperature.")
     },
     {
         "question":
-        ("Which element is most abundant in both Earth’s crust and the human body?"
+        ("4. Which element is most abundant in both Earth’s crust and the human body?"
          " into energy?"),
         "answers":
-        ["iron", "hydrogen", "silicon", "oxygen"],
-        "correct_answer": "oxygen",
+        ["1.iron", "2.hydrogen", "3.silicon", "4.oxygen"],
+        "correct_answer": "4.oxygen",
         "details": "Oxygen constitutes nearly half of Earth’s crust and two-thirds of a human body’s mass."
 
     },
     {
-        "question": "Diamonds are made of which element?",
+        "question": "5. Diamonds are made of which element?",
         "answers":
-        ["sulfur", "nitrogen", "lithium","carbon"],
-        "correct_answer": "carbon",
+        ["1.sulfur", "2.nitrogen", "3.lithium","4.carbon"],
+        "correct_answer": "4.carbon",
         "details": ("Contrary to popular belief, diamonds are not made from coal, although both contain carbon.")
 
     },
     {
-        "question": "What is the most common element found in all living organisms?",
+        "question": "6. What is the most common element found in all living organisms?",
         "answers":
-        ["Oxygen", "Carbon", "Nitrogen", "Hydrogen"],
-        "correct_answer": "Carbon",
+        ["1.Oxygen", "2.Carbon", "3.Nitrogen", "4.Hydrogen"],
+        "correct_answer": "2.Carbon",
         "details": "Contrary to popular belief, diamonds are not made from coal, although both contain carbon."
 
     },
     {
-        "question":"Which element found in red blood cells helps carry oxygen?",
+        "question":"7. Which element found in red blood cells helps carry oxygen?",
         "answers":
-        ["iron", "boron", "calcium","lead"],
-        "correct_answer": "iron",
+        ["1.iron", "2.boron", "3.calcium","4.lead"],
+        "correct_answer": "1.iron",
         "details": ("The iron in hemoglobin forms a reversible bond with oxygen;"
         " they latch together in the lungs and then split at the oxygen’s destination.")
 
     },
     {
-        "question": "Which of these famous poisons appears in the periodic table?",
+        "question": "8. Which of these famous poisons appears in the periodic table?",
         "answers":
-        ["cyanide", "strychnine", "ricin", "arsenic"],
-        "correct_answer": "arsenic",
+        ["1.cyanide", "2.strychnine", "3.ricin", "4.arsenic"],
+        "correct_answer": "4.arsenic",
         "details": "Arsenic has been dubbed the “king of poisons” thanks to its odorless and tasteless qualities."
 
     },
     {
-        "question":"Elements, such as helium and neon, that tend to not undergo chemical reactions are called what?",
+        "question":"9. Elements, such as helium and neon, that tend to not undergo chemical reactions are called what?",
         "answers":
-        ["conservative gases", "lazy gases", "content gases", "noble gases"],
-        "correct_answer": "noble gases",
+        ["1.conservative gases", "2.lazy gases", "3.content gases", "4.noble gases"],
+        "correct_answer": "4.noble gases",
         "details": ("Just as a nobleman might avoid commoners,"
         " so too do noble gases resist interacting with other elements.")
 
     },
     {
-        "question": "Which element’s name and symbol (Cu) comes from the Latin name for Cyprus, where ancient Romans mined it?",
+        "question": "10. Which element’s name and symbol (Cu) comes from the Latin name for Cyprus, where ancient Romans mined it?",
         "answers":
-        ["copper", "chromium", "calcium", "carbon"],
-        "correct_answer": "copper",
+        ["1.copper", "2.chromium", "3.calcium", "4.carbon"],
+        "correct_answer": "4.copper",
         "details": ("The Romans called the element aes Cyprium (“metal of Cyprus”),"
         " which was shortened to cyprium and then corrupted to cuprum, eventually becoming copper.")
 
     },
     {
-        "question": "Which element was discovered on the Sun before it was found on Earth?",
+        "question": "11. Which element was discovered on the Sun before it was found on Earth?",
         "answers":
-        ["helium", "potassium", "neon", "iodine"],
-        "correct_answer": "helium",
+        ["1.helium", "2.potassium", "3.neon", "4.iodine"],
+        "correct_answer": "1.helium",
         "details": ("Using a spectroscope in 1868, scientists first detected helium on the Sun (from helios, the Greek word for “sun”) during an eclipse."
         " Fourteen years later, helium was found on Earth.")
 
     },
     {
-        "question": "Ancient Romans used which element to make plumbing?",
+        "question": "12. Ancient Romans used which element to make plumbing?",
         "answers":
-        ["gold", "titanium", "lead", "aluminum"],
-        "correct_answer": "lead",
+        ["1.gold", "2.titanium", "3.lead", "4.aluminum"],
+        "correct_answer": "3.lead",
         "details": ("Because it is highly durable and resistant to corrosion, lead (chemical symbol Pb from the Latin plumbum) "
         "was used by the Romans to make water pipes as well as coins and cooking utensils."
         " By the time of Augustus Caesar, lead poisoning had become an issue in Rome.")
 
     },
     {
-        "question": "Which of these letters does not appear in the periodic table, either as a symbol or in an element name?",
+        "question": "13. Which of these letters does not appear in the periodic table, either as a symbol or in an element name?",
         "answers":
-        ["k", "x", "z", "j"],
-        "correct_answer": "j",
+        ["1. k", "2. x", "3. z", "4. j"],
+        "correct_answer": "4. j",
         "details": ("The letter x can be found in xenon, z in zinc, and k in krypton (among other places)."
         " The letter q appears only in the extended periodic table in the name unbiquadium, "
         "a placeholder term for the as yet undiscovered element that will have the atomic number 124.")
@@ -123,22 +123,22 @@ questions = [
     },
     {
         "question":
-        ("Frequently used in flares, which element burns so intensely that it can "
+        ("14. Frequently used in flares, which element burns so intensely that it can "
         "cause temporary blindness and cannot be extinguished with water?"),
         "answers":
-        ["magnesium", "neon", "iodine", "silicon"],
-        "correct_answer": "magnesium",
+        ["1.magnesium", "2.neon", "3.iodine", "4.silicon"],
+        "correct_answer": "1.magnesium",
         "details": ("Magnesium has been used in flash photography, pyrotechnics, and incendiary bombs."
         "When introduced to a magnesium fire, water is converted to a hydrogen gas, further fueling the flame.")
 
     },
      {
         "question":
-        ("Used by the Indigenous people of South America for metalcraft,"
+        ("15. Used by the Indigenous people of South America for metalcraft,"
         " which element comes from the Spanish word for “silver”?"),
         "answers":
-        ["tin", "cobalt", "nickel", "platinum"],
-        "correct_answer": "platinum",
+        ["1.tin", "2.cobalt", "3.nickel", "4.platinum"],
+        "correct_answer": "4.platinum",
         "details": ("Spaniards who arrived in South America found deposits of the metal in Colombia’s "
         "Río Pinto and called it platina del Pinto (“silver of the Pinto”).")
 
@@ -197,12 +197,36 @@ def input_value():
     user_input  = input("First, please enter your name:  ")
     return user_input
 
+def check_answer_input():
+    answer_input  = input("Enter answer number:  ")
+    while true:
+        try:
+            answer_number = int(answer_input)
+            if answer_number < 1 or answer_number > 4:
+                raise ValueError(
+                    f"answer option should be from 1 to 4"
+                )
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            
+        
+
+
 def run_quiz():
-    print(questions)
+    for index in (questions):
+        print(index['question \n'])
+        for answers in index['answers']:
+            print(answers)
+        check_answer_input()
+        
+            
+       
+
 
 
 def main():
-    quiz_introduction()
+    #quiz_introduction()
+    run_quiz()
     
   
 main()
